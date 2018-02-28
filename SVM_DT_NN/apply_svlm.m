836cebf63931f29a0015aed5caee60d00aacefa4
Angrrr_ <li.ang198744@gmail.com>
2018-02-28 14:40:37 -0500

    update

--- SVM_DT_NN/apply_svlm.m
+++ SVM_DT_NN/apply_svlm.m
@@ -1,6 +1,10 @@
-function [ class ] = apply_svlm( x, w, b )
-%this function
+function [ y_hat ] = apply_svlm( x, w, b )
+%this function take the new input data x
+% w, b is trained from svml
+% output the predicted class
 
+y_hat = x*w +b > 0;
+y_sore = x*w +b;
 
 end
 
--- SVM_DT_NN/p1.m
+++ SVM_DT_NN/p1.m
@@ -1,9 +1,58 @@
 %% input data
 load pima_train.txt
+load pima_test.txt
+
 X_train = pima_train(:, 1:8);
 y_train = pima_train(:, 9);
 [w, b] = svml(X_train,Y_train,1);
-w
-b
 
-%%
+X_test = pima_test(:, 1:8);
+y_test = pima_test(:, 9);
+
+%% Part c. for trainning set
+Y_train_predict = [];
+for i =1:length(X_train)
+    x = X_train(i, :);
+    y_hat = apply_svlm( x, w, b );
+    Y_train_predict = [Y_train_predict, y_hat];
+end
+
+Y_train_class = y_train;
+Y_train_predict = Y_train_predict';
+
+%% training confusion matrix
+conf_mat_train = confusionmat(Y_train_class,Y_train_predict,'order',[1,0])
+
+TP = conf_mat_train(1,1);
+TN = conf_mat_train(2,2);
+FN = conf_mat_train(2,1);
+FP = conf_mat_train(1,2);
+accuracy_train = (TP+TN)/sum(sum(conf_mat_train));
+misclass_train = 1-accuracy_train
+sensit = TP/(TP+FN)
+specif = TN/(TN+FP)
+
+%% for testing set
+Y_test_predict = [];
+for i =1:length(X_test)
+    x = X_train(i, :);
+    y_hat = apply_svlm( x, w, b );
+    Y_test_predict = [Y_test_predict, y_hat];  
+end
+
+Y_test_class = y_test;
+Y_test_predict = Y_test_predict';
+
+%% testing confusion matrix
+conf_mat_test = confusionmat(Y_test_class,Y_test_predict,'order',[1,0]);
+
+TP = conf_mat_test(1,1);
+TN = conf_mat_test(2,2);
+FN = conf_mat_test(2,1);
+FP = conf_mat_test(1,2);
+accuracy_test = (TP+TN)/sum(sum(conf_mat_test));
+misclass_test = 1-accuracy_test
+test_sensit = TP/(TP+FN)
+test_specif = TN/(TN+FP)
+
+%% ROC curve
